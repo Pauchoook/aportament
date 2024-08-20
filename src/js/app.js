666916ee9c39e2cb04564b74bfb82fd2d6b2ceb6
Pauchoook <nkolatarasov@mail.ru>
2024-08-20 23:55:52 +0300

    repair burger

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,8 @@ import * as functions from "./files/functions.js";
 
 import "../scss/style.scss";
 import spoller from "./files/spoller.js";
+import burger from "./files/burger.js";
 
 functions.isWebp();
-functions.mediaAdaptive();
 spoller();
+burger();
